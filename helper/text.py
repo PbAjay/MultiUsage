9b8efcc7dc12744b31f572d56213ee28107e6e3f
AjayZ TG <106470358+PbAjay@users.noreply.github.com>
2023-07-10 12:38:17 +0530

    Update text.py

--- helper/text.py
+++ helper/text.py
@@ -95,18 +95,13 @@ sᴇɴᴅ ᴄʜᴀɴɴᴇʟ ʟᴀsᴛ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ғᴏʀᴡᴇ�
 /logo MLZ
 /logosq Ajay  """
 
-    YTDL = """<b>𝚈𝙾𝚄𝚃𝚄𝙱𝙴 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 𝙼𝙾𝙳𝚄𝙻𝙴</b>
+    YTDL = """ʜᴇʟᴘ: ʏᴛ ᴅʟ
 
-🍁 𝘜𝘴𝘢𝘨𝘦
+ʏᴏᴜ ᴄᴀɴ ᴅᴏᴡɴʟᴏᴀᴅ ʏᴏᴜᴛᴜʙᴇ ᴠɪᴅᴇᴏ ᴜsɪɴɢ ɪs ғᴇᴀᴛᴜʀᴇ
 
-𝘠𝘰𝘶 𝘊𝘢𝘯 𝘋𝘰𝘸𝘯𝘭𝘰𝘢𝘥 𝘈𝘯𝘺 𝘝𝘪𝘥𝘦𝘰 𝘖𝘳 𝘈𝘶𝘥𝘪𝘰 𝘍𝘳𝘰𝘮 𝘠𝘰𝘶𝘵𝘶𝘣𝘦
-
-𝙃𝙤𝙬 𝙏𝙤 𝙐𝙨𝙚
-
-• /song 𝚂𝙾𝙽𝙶 𝙽𝙰𝙼𝙴 
-• /video or /mp4 𝘈𝘯𝘥 https://youtu.be/*****
-• 𝘌𝘹𝘢𝘮𝘱𝘭𝘦:
-<code>/song mkn</code>
+ᴄᴏᴍᴍᴀɴᴅs ᴀɴᴅ ᴜsᴀɢᴇ
+/video ᴏʀ /mp4 ᴀɴᴅ https://youtu.be/*****
+ᴇxᴀᴍᴘʟᴇ:
 <code>/mp4 https://youtu.be/*******</code>
 <code>/video https://youtu.be/*****</code>  """
   
