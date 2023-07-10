9fa6b24918e7350dfc6d5d82b6600e4a8baaadd8
AjayZ TG <106470358+PbAjay@users.noreply.github.com>
2023-07-10 07:51:07 +0530

    Create logo.py

---
+++ plugins/logo.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters, idle
+import pyrogram, asyncio, random, time
+from pyrogram.errors import FloodWait
+from pyrogram.types import *
+import requests
+
+
+@Client.on_message(filters.command("logo"))
+async def logo(bot, msg: Message):
+    if len(message.command) == 1:
+       return await msg.reply_text("ᴜsᴀɢᴇ:\n\n /logo MLZ")
+    logo_name = msg.text.split(" ", 1)[1]
+    API = f"https://api.sdbots.tk/logohq?text={logo_name}"
+    req = requests.get(API).url
+    await msg.reply_photo(
+        photo=f"{req}")
